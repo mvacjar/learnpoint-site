d87de7186276f13dcb3c9a2fa4d1defe8e6d5380
mvacjar <mvacjar@gmail.com>
2024-10-14 11:39:35 +0200

    dev-dark-mode

---
+++ docs/_scripts/components/color-mode.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('.footer__select-light-mode-button')) {
+      applyColorMode('light-mode');
+    } else if (event.target.matches('.footer__select-dark-mode-button')) {
+      applyColorMode('dark-mode');
+    }
+  });
+  detectPreferredColorScheme();
+});
+
+function detectPreferredColorScheme() {
+  const savedMode = localStorage.getItem('selected-color-mode');
+  if (savedMode) {
+    applyColorMode(savedMode);
+  } else if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  ) {
+    applyColorMode('dark-mode');
+  } else {
+    applyColorMode('light-mode');
+  }
+}
+
+function applyColorMode(mode) {
+  document.body.className = mode;
+  localStorage.setItem('selected-color-mode', mode);
+}
