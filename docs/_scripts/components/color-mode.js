7b638557432b1c4ee5e1b2c7b03d2362c2c3884b
mvacjar <mvacjar@gmail.com>
2024-10-23 12:05:34 +0200

    Conflict CSS problem and the screen size fixed

--- docs/_scripts/components/color-mode.js
+++ docs/_scripts/components/color-mode.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   detectPreferredColorScheme();
-  detectScreenMode();
 });
 
 document.addEventListener('click', (event) => {
@@ -12,11 +11,9 @@ document.addEventListener('click', (event) => {
 });
 
 function applyColorMode(mode) {
-  document.documentElement.className = mode;
-  document.body.className = mode;
+  document.documentElement.classList.remove('is-lightmode', 'is-darkmode');
+  document.documentElement.classList.add(mode);
   localStorage.setItem('selected-color-mode', mode);
-
-  updateLogo();
 }
 
 function detectPreferredColorScheme() {
@@ -24,58 +21,9 @@ function detectPreferredColorScheme() {
 
   if (savedMode) {
     applyColorMode(savedMode);
-  } else if (
-    window.matchMedia &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-  ) {
+  } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     applyColorMode('is-darkmode');
   } else {
     applyColorMode('is-lightmode');
-  }
-}
-
-function detectScreenMode() {
-  const MOBILE_WIDTH = 768;
-  const query = window.matchMedia(`(max-width: ${MOBILE_WIDTH}px)`);
-
-  query.addEventListener('change', setScreenMode);
-  setScreenMode();
-
-  function setScreenMode() {
-    if (query.matches) {
-      document.documentElement.classList.add('is-mobile');
-      document.documentElement.classList.remove('is-desktop');
-    } else {
-      document.documentElement.classList.add('is-desktop');
-      document.documentElement.classList.remove('is-mobile');
-    }
-
-    updateLogo();
-  }
-}
-
-function updateLogo() {
-  const mode = localStorage.getItem('selected-color-mode') || 'is-lightmode';
-  const isMobile = document.documentElement.classList.contains('is-mobile');
-
-  document
-    .querySelectorAll('.header__logo-desktop-img, .header__logo-mobile-img')
-    .forEach((img) => {
-      img.style.display = 'none';
-    });
-
-  if (mode === 'is-darkmode') {
-    if (isMobile) {
-      document.querySelector('.dark-mobile-mode-logo').style.display = 'block';
-    } else {
-      document.querySelector('.dark-desktop-mode-logo').style.display = 'block';
-    }
-  } else {
-    if (isMobile) {
-      document.querySelector('.light-mobile-mode-logo').style.display = 'block';
-    } else {
-      document.querySelector('.light-desktop-mode-logo').style.display =
-        'block';
-    }
   }
 }
