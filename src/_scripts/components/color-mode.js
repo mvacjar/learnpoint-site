66a43ce3b41373ace53a2ba69b2059a33c923274
Nesrinemh <nesrine.hadboun@live.fr>
2024-10-29 20:17:50 +0100

    setup toggle dark icon

--- src/_scripts/components/color-mode.js
+++ src/_scripts/components/color-mode.js
@@ -1,47 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   detectPreferredColorScheme();
-// });
-
-// document.addEventListener('click', (event) => {
-//   if (event.target.matches('.footer__select-light-mode-button')) {
-//     applyColorScheme('is-light');
-//   } else if (event.target.matches('.footer__select-dark-mode-button')) {
-//     applyColorScheme('is-dark');
-//   }
-// });
-
-// function applyColorScheme(mode) {
-//   document.documentElement.classList.remove('is-light', 'is-dark');
-//   document.documentElement.classList.add(mode);
-//   localStorage.setItem('selected-color-mode', mode);
-// }
-
-// function detectPreferredColorScheme() {
-//   const savedMode = localStorage.getItem('selected-color-mode');
-
-//   if (savedMode) {
-//     applyColorScheme(savedMode);
-//   } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//     applyColorScheme('is-dark');
-//   } else {
-//     applyColorScheme('is-light');
-//   }
-// }
-
-// (function () {
-//   const savedMode = localStorage.getItem('selected-color-mode');
-//   if (savedMode) {
-//     document.documentElement.classList.add(savedMode);
-//   } else if (
-//     window.matchMedia &&
-//     window.matchMedia('(prefers-color-scheme: dark)').matches
-//   ) {
-//     document.documentElement.classList.add('is-dark');
-//   } else {
-//     document.documentElement.classList.add('is-light');
-//   }
-// })();
-
 (function () {
   const applyColorScheme = (mode) => {
     document.documentElement.classList.remove('is-light', 'is-dark');
@@ -54,7 +10,9 @@
 
     if (savedMode) {
       applyColorScheme(savedMode);
-    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    } else if (
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
       applyColorScheme('is-dark');
     } else {
       applyColorScheme('is-light');
@@ -73,9 +31,16 @@
   });
 
   document.addEventListener('click', (event) => {
-    if (event.target.matches('.footer__select-light-mode-button')) {
+    const lightButton = event.target.closest(
+      '.footer__select-light-mode-button'
+    );
+    const darkButton = event.target.closest(
+      '.footer__select-dark-mode-button'
+    );
+
+    if (lightButton) {
       applyColorScheme('is-light');
-    } else if (event.target.matches('.footer__select-dark-mode-button')) {
+    } else if (darkButton) {
       applyColorScheme('is-dark');
     }
   });
